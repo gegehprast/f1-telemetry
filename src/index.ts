c3404ac7dc3a6566acabb394d7009fa55b40aa4a
gegehprast <gegeh.prast@gmail.com>
2022-08-12 13:42:42 +0700

    move index

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import App from './App'
+
+// handle unhandledRejection error event
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('[APP] Unhandled Rejection at:', (reason as any).stack || reason, promise)
+})
+
+// stops the app
+;[
+    'exit',
+    'SIGINT',
+    'SIGUSR1',
+    'SIGUSR2',
+    'uncaughtException',
+    'SIGTERM',
+].forEach((eventType) => {
+    ;(process as NodeJS.EventEmitter).on(eventType, () => App.stop())
+})
+
+
+// start app
+App.start()
