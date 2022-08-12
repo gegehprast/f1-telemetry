11c8e2085922f68c931ddd161756a2a97c6abfb4
gegehprast <gegeh.prast@gmail.com>
2022-08-12 14:58:42 +0700

    handlers

--- src/services/PacketHandler.ts
+++ src/services/PacketHandler.ts
@@ -1,49 +1,123 @@
-type Listener = (...args: any[]) => void
+import { PacketEventData } from "@racehub-io/f1-telemetry-client/build/src/parsers/packets/types"
+import CarDamage from "../models/CarDamage"
+import CarSetup from "../models/CarSetup"
+import CarStatus from "../models/CarStatus"
+import CarTelemetry from "../models/CarTelemetry"
+import Event from "../models/Event"
+import FinalClassification from "../models/FinalClassification"
+import LapData from "../models/LapData"
+import LobbyInfo from "../models/LobbyInfo"
+import Motion from "../models/Motion"
+import Participant from "../models/Participant"
+import Session from "../models/Session"
+import SessionHistory from "../models/SessionHistory"
 
-export const eventHandler: Listener = (event) => {
-    console.log({ name: 'event', event })
+type Listener = (...args: any[]) => Promise<void>
+
+export const eventHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new Event(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const motionHandler: Listener = (event) => {
-    console.log({ name: 'motion', event })
+export const motionHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new Motion(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const carSetupsHandler: Listener = (event) => {
-    console.log({ name: 'carSetups', event })
+export const carSetupsHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new CarSetup(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const lapDataHandler: Listener = (event) => {
-    console.log({ name: 'lapData', event })
+export const lapDataHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new LapData(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const sessionHandler: Listener = (event) => {
-    console.log({ name: 'session', event })
+export const sessionHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new Session(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const participantsHandler: Listener = (event) => {
-    console.log({ name: 'participants', event })
+export const participantsHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new Participant(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const carTelemetryHandler: Listener = (event) => {
-    console.log({ name: 'carTelemetry', event })
+export const carTelemetryHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new CarTelemetry(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const carStatusHandler: Listener = (event) => {
-    console.log({ name: 'carStatus', event })
+export const carStatusHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new CarStatus(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const finalClassificationHandler: Listener = (event) => {
-    console.log({ name: 'finalClassification', event })
+export const finalClassificationHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new FinalClassification(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const lobbyInfoHandler: Listener = (event) => {
-    console.log({ name: 'lobbyInfo', event })
+export const lobbyInfoHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new LobbyInfo(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const carDamageHandler: Listener = (event) => {
-    console.log({ name: 'carDamage', event })
+export const carDamageHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new CarDamage(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const sessionHistoryHandler: Listener = (event) => {
-    console.log({ name: 'sessionHistory', event })
+export const sessionHistoryHandler: Listener = async (event: PacketEventData) => {
+    try {
+        const doc = new SessionHistory(event)
+        await doc.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
