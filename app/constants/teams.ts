27723256900afccaf833f1610b012d50b90e4a00
gegehprast <gegeh.prast@gmail.com>
2022-08-13 14:00:49 +0700

    constants

---
+++ app/constants/teams.ts
@@ -0,0 +1,122 @@
+const MERCEDES_COLOR = '#00D2BE'
+const FERRARI_COLOR = '#DC0000'
+const RED_BULL_COLOR = '#0600EF'
+const WILLIAMS_COLOR = '#005AFF'
+const ASTON_MARTIN_COLOR = '#006F62'
+const ALPINE_COLOR = '#0090FF'
+const ALPHA_TAURI_COLOR = '#2B4562'
+const HAAS_COLOR = '#FFFFFF'
+const MCLAREN_COLOR = '#FF8700'
+const ALFA_ROMEO_COLOR = '#900000'
+const LOTUS_COLOR = '#fcd56d'
+const JORDAN_COLOR = '#006F62'
+const BENTETTON_COLOR = '#fcd56d'
+const ART_GRAND_PRIX_COLOR = '#B4B3B4'
+const CAMPOS_VEXATEC_COLOR = '#EBC110'
+const CARLIN_COLOR = '#243EF6'
+const CHAROUZ_COLOR = '#84020A'
+const DAMS_COLOR = '#0ED4FA'
+const RUSSIAN_TIME_COLOR = '#181e2a'
+const MP_MOTORSPORT_COLOR = '#F7401A'
+const PERTAMINA_COLOR = '#ff2b08'
+const TRIDENT_COLOR = '#0E1185'
+const BWT_ARDEN_COLOR = '#ff88d3'
+const UNI_VIRTUOSI_COLOR = '#FBEC20'
+const PREMA_COLOR = '#E80309'
+const HITECH_COLOR = '#E8E8E8'
+const UNI_VIRTUOSI_COLOR_19 = '#ffd600'
+const PREMA_COLOR_19 = '#db1500'
+const ART_GRAND_PRIX_COLOR_19 = '#640005'
+const CAMPOS_VEXATEC_COLOR_19 = '#FA6400'
+const CARLIN_COLOR_19 = '#0A505A'
+const CHAROUZ_COLOR_19 = '#840D2B'
+const DAMS_COLOR_19 = '#00E1EB'
+const MP_MOTORSPORT_COLOR_19 = '#F4736C'
+const TRIDENT_COLOR_19 = '#2D5AD7'
+const BWT_ARDEN_COLOR_19 = '#FAA0BE'
+const RACING_POINT_COLOR_2020 = '#FAA0BE'
+const RENAULT_COLOR_2020 = '#FFF500'
+
+export const TEAMS = [
+    { name: 'Mercedes', color: MERCEDES_COLOR },
+    { name: 'Ferrari', color: FERRARI_COLOR },
+    { name: 'Red Bull Racing', color: RED_BULL_COLOR },
+    { name: 'Williams', color: WILLIAMS_COLOR },
+    { name: 'Aston Martin', color: ASTON_MARTIN_COLOR },
+    { name: 'Alpine', color: ALPINE_COLOR },
+    { name: 'Alpha Tauri', color: ALPHA_TAURI_COLOR },
+    { name: 'Haas', color: HAAS_COLOR },
+    { name: 'McLaren', color: MCLAREN_COLOR },
+    { name: 'Alfa Romeo', color: ALFA_ROMEO_COLOR },
+    { name: 'McLaren 1988', color: MCLAREN_COLOR },
+    { name: 'McLaren 1991', color: MCLAREN_COLOR },
+    { name: 'Williams 1992', color: WILLIAMS_COLOR },
+    { name: 'Ferrari 1995', color: FERRARI_COLOR },
+    { name: 'Williams 1996', color: WILLIAMS_COLOR },
+    { name: 'McLaren 1998', color: MCLAREN_COLOR },
+    { name: 'Ferrari 2002', color: FERRARI_COLOR },
+    { name: 'Ferrari 2004', color: FERRARI_COLOR },
+    { name: 'Alpine 2006', color: ALPINE_COLOR },
+    { name: 'Ferrari 2007', color: FERRARI_COLOR },
+    { name: 'INVALID', color: 'INVALID' },
+    { name: 'Red Bull 2010', color: RED_BULL_COLOR },
+    { name: 'Ferrari 1976', color: FERRARI_COLOR },
+    { name: 'ART Grand Prix', color: ART_GRAND_PRIX_COLOR },
+    { name: 'Campos Vexatec Racing', color: CAMPOS_VEXATEC_COLOR },
+    { name: 'Carlin', color: CARLIN_COLOR },
+    { name: 'Charouz Racing System', color: CHAROUZ_COLOR },
+    { name: 'DAMS', color: DAMS_COLOR },
+    { name: 'Russian Time', color: RUSSIAN_TIME_COLOR },
+    { name: 'MP Motorsport', color: MP_MOTORSPORT_COLOR },
+    { name: 'Pertamina', color: PERTAMINA_COLOR },
+    { name: 'McLaren 1990', color: MCLAREN_COLOR },
+    { name: 'Trident', color: TRIDENT_COLOR },
+    { name: 'BWT Arden', color: BWT_ARDEN_COLOR },
+    { name: 'McLaren 1976', color: MCLAREN_COLOR },
+    { name: 'Lotus 1972', color: LOTUS_COLOR },
+    { name: 'Ferrari 1979', color: FERRARI_COLOR },
+    { name: 'McLaren 1982', color: MCLAREN_COLOR },
+    { name: 'Williams 2003', color: WILLIAMS_COLOR },
+    { name: 'Brawn 2009', color: MCLAREN_COLOR },
+    { name: 'Lotus 1978', color: LOTUS_COLOR },
+    { name: 'INVALID', color: 'INVALID' },
+    { name: 'Art GP ’19', color: ART_GRAND_PRIX_COLOR_19 },
+    { name: 'Campos ’19', color: CAMPOS_VEXATEC_COLOR_19 },
+    { name: 'Carlin ’19', color: CARLIN_COLOR_19 },
+    { name: 'Sauber Junior Charouz ’19', color: CHAROUZ_COLOR_19 },
+    { name: 'Dams ’19', color: DAMS_COLOR_19 },
+    { name: 'Uni-Virtuosi ‘19', color: UNI_VIRTUOSI_COLOR_19 },
+    { name: 'MP Motorsport ‘19', color: MP_MOTORSPORT_COLOR_19 },
+    { name: 'Prema ’19', color: PREMA_COLOR_19 },
+    { name: 'Trident ’19', color: TRIDENT_COLOR_19 },
+    { name: 'Arden ’19', color: BWT_ARDEN_COLOR_19 },
+    { name: 'Benetton 1994', color: BENTETTON_COLOR },
+    { name: 'Benetton 1995', color: BENTETTON_COLOR },
+    { name: 'Ferrari 2000', color: MCLAREN_COLOR },
+    { name: 'Jordan 1991', color: JORDAN_COLOR },
+    { name: 'Ferrari 1990', color: FERRARI_COLOR },
+    { name: 'McLaren 2010', color: MCLAREN_COLOR },
+    { name: 'Ferrari 2010', color: FERRARI_COLOR },
+    { name: 'Art GP ’20', color: ART_GRAND_PRIX_COLOR },
+    { name: 'Campos ’20', color: CAMPOS_VEXATEC_COLOR },
+    { name: 'Carlin ’20', color: CARLIN_COLOR },
+    { name: 'Charouz ’20', color: CHAROUZ_COLOR },
+    { name: 'Dams ’20', color: DAMS_COLOR },
+    { name: 'Uni-Virtuosi ’20', color: UNI_VIRTUOSI_COLOR },
+    { name: 'MP Motorsport ’20', color: MP_MOTORSPORT_COLOR },
+    { name: 'Prema ’20', color: PREMA_COLOR },
+    { name: 'Trident ’20', color: TRIDENT_COLOR },
+    { name: 'BWT ’20', color: BWT_ARDEN_COLOR },
+    { name: 'Hitech ’20', color: HITECH_COLOR },
+    { name: 'Mercedes 2020', color: HITECH_COLOR },
+    { name: 'Ferrari 2020', color: FERRARI_COLOR },
+    { name: 'Red Bull 2020', color: RED_BULL_COLOR },
+    { name: 'Williams 2020', color: WILLIAMS_COLOR },
+    { name: 'Racing Point 2020', color: RACING_POINT_COLOR_2020 },
+    { name: 'Renault 2020', color: RENAULT_COLOR_2020 },
+    { name: 'Alpha Tauri 2020', color: ALPHA_TAURI_COLOR },
+    { name: 'Haas 2020', color: HAAS_COLOR },
+    { name: 'McLaren 2020', color: MCLAREN_COLOR },
+    { name: 'Alfa Romeo 2020', color: ALFA_ROMEO_COLOR },
+    { name: 'Not found', color: '#FFFFFF' },
+]
