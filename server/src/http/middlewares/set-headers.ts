7b7c696b6b459a410e23941c0e1add5134fdc0c2
gegehprast <gegeh.prast@gmail.com>
2022-08-13 15:39:10 +0700

    *

--- server/src/http/middlewares/set-headers.ts
+++ server/src/http/middlewares/set-headers.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express'
 
 const setHeaders: RequestHandler = (req, res, next) => {
     res.set('X-Powered-By', 'Whatsapp Automation')
-    res.append('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS || 'http://localhost:3000')
+    res.append('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS || '*')
     res.append('Access-Control-Allow-Credentials', 'true')
     res.append('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE'])
     res.append('Access-Control-Allow-Headers', ['Content-Type', 'Authorization', 'Accept'])
