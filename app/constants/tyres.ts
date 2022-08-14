ecd600ce918a458d77a508c5ead7948bda041264
gegehprast <gegeh.prast@gmail.com>
2022-08-14 20:17:54 +0700

    tyres

--- app/constants/tyres.ts
+++ app/constants/tyres.ts
@@ -1,23 +1,23 @@
 export const TYRES = [
-    { color: '#ffb3c3', name: 'Hyper Soft' },
-    { color: '#b14ba7', name: 'Ultra Soft' },
-    { color: '#f92d29', name: 'Super Soft' },
-    { color: '#ebd25f', name: 'Soft' },
-    { color: '#ffffff', name: 'Medium' },
-    { color: '#03a2f3', name: 'Hard' },
-    { color: '#ff803f', name: 'Super Hard' },
-    { color: '#3ac82b', name: 'Intermediate' },
-    { color: '#4491d2', name: 'Wet' },
-    { color: '#3ac82b', name: 'Dry' },
-    { color: '#4491d2', name: 'Wet' },
-    { color: '#f92d29', name: 'Super Soft' },
-    { color: '#f92d29', name: 'Soft' },
-    { color: '#ebd25f', name: 'Medium' },
-    { color: '#ffffff', name: 'Hard' },
-    { color: '#4491d2', name: 'Wet' },
-    { color: '#f92d29', name: 'C5' },
-    { color: '#f92d29', name: 'C4' },
-    { color: '#ebd25f', name: 'C3' },
-    { color: '#ffffff', name: 'C2' },
-    { color: '#ffffff', name: 'C1' },
+    { color: '#ffb3c3', name: 'Hyper Soft' }, // 0
+    { color: '#b14ba7', name: 'Ultra Soft' }, // 1
+    { color: '#f92d29', name: 'Super Soft' }, // 2
+    { color: '#ebd25f', name: 'Soft' }, // 3
+    { color: '#ffffff', name: 'Medium' }, // 4
+    { color: '#03a2f3', name: 'Hard' }, // 5
+    { color: '#ff803f', name: 'Super Hard' }, // 6
+    { color: '#3ac82b', name: 'Intermediate' }, // 7
+    { color: '#4491d2', name: 'Wet' }, // 8
+    { color: '#3ac82b', name: 'Dry' }, // 9
+    { color: '#4491d2', name: 'Wet' }, // 10
+    { color: '#f92d29', name: 'Super Soft' }, // 11
+    { color: '#f92d29', name: 'Soft' }, // 12
+    { color: '#ebd25f', name: 'Medium' }, // 13
+    { color: '#ffffff', name: 'Hard' }, // 14
+    { color: '#4491d2', name: 'Wet' }, // 15
+    { color: '#f92d29', name: 'C5' }, // 16
+    { color: '#f92d29', name: 'C4' }, // 17
+    { color: '#ebd25f', name: 'C3' }, // 18
+    { color: '#ffffff', name: 'C2' }, // 19
+    { color: '#ffffff', name: 'C1' }, // 20
 ]
