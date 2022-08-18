bef53285a3cd039ede4b14271b3e0ce873357998
gegehprast <gegeh.prast@gmail.com>
2022-08-18 17:14:18 +0700

    getBestTimesData

--- app/helpers/sessionparser.ts
+++ app/helpers/sessionparser.ts
@@ -6,11 +6,11 @@ import { TRACKS } from "../constants/track";
 import { ISessionDoc, LapHistoryData } from "../Types";
 import { convertDuration } from "./time";
 
-const getBestTime = (
+const getBestTimeFromLapHistory = (
     m_lapHistoryData: LapHistoryData[],
     key: keyof LapHistoryData
 ) => {
-    const sorted = m_lapHistoryData.sort((a, b) => {
+    const sorted = [...m_lapHistoryData].sort((a, b) => {
         if (a[key] < b[key]) {
             return -1
         }
@@ -22,7 +22,7 @@ const getBestTime = (
         return 0
     })
 
-    return sorted[0]
+    return sorted[0] 
 }
 
 const FLAGS = {
@@ -43,36 +43,51 @@ const checkFlag = (num: number) => {
     return selected
 }
 
+const getBestTimesData = (m_lapHistoryData: LapHistoryData[]) => {
+    if (m_lapHistoryData.length) {
+        const bestLapTimeData = getBestTimeFromLapHistory(
+            m_lapHistoryData,
+            'm_lapTimeInMS'
+        )
+        const bestSector1TimeData = getBestTimeFromLapHistory(
+            m_lapHistoryData,
+            'm_sector1TimeInMS'
+        )
+        const bestSector2TimeData = getBestTimeFromLapHistory(
+            m_lapHistoryData,
+            'm_sector2TimeInMS'
+        )
+        const bestSector3TimeData = getBestTimeFromLapHistory(
+            m_lapHistoryData,
+            'm_sector3TimeInMS'
+        )
+        
+        return {
+            bestLapTime: bestLapTimeData.m_lapTimeInMS,
+            bestSector1Time: bestSector1TimeData.m_sector1TimeInMS,
+            bestSector2Time: bestSector2TimeData.m_sector2TimeInMS,
+            bestSector3Time: bestSector3TimeData.m_sector3TimeInMS,
+            bestLapTimeValid: checkFlag(bestLapTimeData.m_lapValidBitFlags || 0).includes('lap'),
+            bestSector1TimeValid: checkFlag(bestSector1TimeData.m_lapValidBitFlags || 0).includes('sector1'),
+            bestSector2TimeValid: checkFlag(bestSector2TimeData.m_lapValidBitFlags || 0).includes('sector2'),
+            bestSector3TimeValid: checkFlag(bestSector3TimeData.m_lapValidBitFlags || 0).includes('sector3'),
+        }
+    }
+
+    return {
+        bestLapTime: 0,
+        bestSector1Time: 0,
+        bestSector2Time: 0,
+        bestSector3Time: 0,
+        bestLapTimeValid: true,
+        bestSector1TimeValid: true,
+        bestSector2TimeValid: true,
+        bestSector3TimeValid: true,
+    }
+}
+
 export const parseSession = (session: ISessionDoc) => {
-    const bestLapTimeData = getBestTime(
-        session.sessionHistory.m_lapHistoryData,
-        'm_lapTimeInMS'
-    )
-    const bestSector1TimeData = getBestTime(
-        session.sessionHistory.m_lapHistoryData,
-        'm_sector1TimeInMS'
-    )
-    const bestSector2TimeData = getBestTime(
-        session.sessionHistory.m_lapHistoryData,
-        'm_sector2TimeInMS'
-    )
-    const bestSector3TimeData = getBestTime(
-        session.sessionHistory.m_lapHistoryData,
-        'm_sector3TimeInMS'
-    )
-    const bestLapTimeValid = checkFlag(
-        bestLapTimeData.m_lapValidBitFlags || 0
-    ).includes('lap')
-    const bestSector1TimeValid = checkFlag(
-        bestSector1TimeData.m_lapValidBitFlags || 0
-    ).includes('sector1')
-    const bestSector2TimeValid = checkFlag(
-        bestSector2TimeData.m_lapValidBitFlags || 0
-    ).includes('sector2')
-    const bestSector3TimeValid = checkFlag(
-        bestSector3TimeData.m_lapValidBitFlags || 0
-    ).includes('sector3')
-    
+    const bestTimesData = getBestTimesData(session.sessionHistory.m_lapHistoryData)
     const player = session.participants.find(
         (participant) => participant.carIndex === session.m_playerCarIndex
     )
@@ -102,21 +117,14 @@ export const parseSession = (session: ISessionDoc) => {
         driver: driver,
         type: SESSION_TYPES[session.m_sessionType].long,
         totalLap: session.m_totalLaps,
-        bestLapTime:
-            bestLapTimeData && convertDuration(bestLapTimeData.m_lapTimeInMS),
-        bestSector1Time:
-            bestLapTimeData &&
-            convertDuration(bestLapTimeData.m_sector1TimeInMS),
-        bestSector2Time:
-            bestLapTimeData &&
-            convertDuration(bestLapTimeData.m_sector2TimeInMS),
-        bestSector3Time:
-            bestLapTimeData &&
-            convertDuration(bestLapTimeData.m_sector3TimeInMS),
-        bestLapTimeValid: bestLapTimeValid,
-        bestSector1TimeValid: bestSector1TimeValid,
-        bestSector2TimeValid: bestSector2TimeValid,
-        bestSector3TimeValid: bestSector3TimeValid,
+        bestLapTime: convertDuration(bestTimesData.bestLapTime),
+        bestSector1Time: convertDuration(bestTimesData.bestSector1Time),
+        bestSector2Time: convertDuration(bestTimesData.bestSector2Time),
+        bestSector3Time: convertDuration(bestTimesData.bestSector3Time),
+        bestLapTimeValid: bestTimesData.bestLapTimeValid,
+        bestSector1TimeValid: bestTimesData.bestSector1TimeValid,
+        bestSector2TimeValid: bestTimesData.bestSector2TimeValid,
+        bestSector3TimeValid: bestTimesData.bestSector3TimeValid,
         date: new Date(
             new Date(session.createdAt).getTime() - session.m_sessionTime
         ),
